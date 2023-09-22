b67c96dc98657bf9d7b1bfe3ad5524c9f9437f8e
Pawan9856 <124979310+Pawan9856@users.noreply.github.com>
2023-09-22 11:56:34 +0530

    Add files via upload

---
+++ pawan.py
@@ -0,0 +1,11 @@
+
+import matplotlib.pyplot as plt
+
+age=[1,2,3,4,5,6,7]
+weight=[2,3,4,5,1,2,8]
+plt.scatter(age,weight)
+plt.title("Scatter Plot",fontsize=15)
+plt.xlabel("Age",fontsize=15)
+plt.ylabel("Weight",fontsize=15)
+plt.show() 
+
